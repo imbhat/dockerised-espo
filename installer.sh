225642a42b9544f97231106fbbd5f0fbd9b4ab54
Karthik Bhat K <theBuzzyCoder@users.noreply.github.com>
2018-09-27 13:56:08 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
-if [ ! -f $(which apt) ]; then
-  echo "Expecting apt. Please do the work around because apt is not available in non-debian OS.";
+if [ ! -f $(which apt-get) ]; then
+  echo "Expecting apt-get. Please do the work around because apt-get is not available in non-debian OS.";
   echo "Use brew for mac";
   echo "Use apk for alpine";
   echo "Use yum for Fedora";
@@ -13,10 +13,10 @@ fi
 
 if [ ! -f "$(which docker-compose)" ]; then
   echo "docker-compose not found. Installing it now ..."
-  sudo apt install docker-compose
+  sudo apt-get install -y docker-compose
 fi
 
-sudo apt install git
+sudo apt-get install -y git
 
 export WORKDIR=$HOME/dockerised-espo
 mkdir -p $WORKDIR
@@ -33,11 +33,11 @@ git clone git://github.com/espocrm/espocrm.git
 export CRMDIR=$WORKDIR/php7.2-espocrm/espocrm
 cd $CRMDIR
 
-sudo apt install -y python-software-properties
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:ondrej/php
-sudo apt update
-sudo apt install -y php7.2-fpm php7.2-gd php7.2-imap php7.2-mbstring php7.2-json
-sudo apt install -y php7.2-ldap php7.2-curl php7.2-xml php7.2-zip php7.2-mysql php7.2-dev
+sudo apt-get update
+sudo apt-get install -y php7.2-fpm php7.2-gd php7.2-imap php7.2-mbstring php7.2-json
+sudo apt-get install -y php7.2-ldap php7.2-curl php7.2-xml php7.2-zip php7.2-mysql php7.2-dev
 sudo apt install -y php-mailparse php-pear phpunit php-db
 
 if [ -f "$(which composer)" ]; then
@@ -50,7 +50,7 @@ fi
 # Add developer mode to espocrm/data/config.php
 echo "<?php return ['isDeveloperMode' => true]; ?>" > $CRMDIR/data/config.php
 
-sudo apt install nodejs npm
+sudo apt-get install -y nodejs npm
 sudo npm install -g grunt
 
 # Building node_modules in local machine before mount for development enviornment
