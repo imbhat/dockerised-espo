2f1e6f048b13876806e7a7bd0dca10f0124c3b04
Karthik Bhat K <theBuzzyCoder@users.noreply.github.com>
2018-10-02 23:55:10 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,6 @@
+echo "This installer is for only development mode on local machine"
+
+
 if [ ! -f $(which apt-get) ]; then
   echo "Expecting apt-get. Please do the work around because apt-get is not available in non-debian OS.";
   echo "Use brew for mac";
@@ -18,6 +21,7 @@ fi
 
 sudo apt-get install -y git
 
+set -x
 export WORKDIR=$HOME/dockerised-espo
 mkdir -p $WORKDIR
 
@@ -33,31 +37,8 @@ git clone git://github.com/espocrm/espocrm.git
 export CRMDIR=$WORKDIR/php7.2-espocrm/espocrm
 cd $CRMDIR
 
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php7.2-fpm php7.2-gd php7.2-imap php7.2-mbstring php7.2-json
-sudo apt-get install -y php7.2-ldap php7.2-curl php7.2-xml php7.2-zip php7.2-mysql php7.2-dev
-sudo apt install -y php-mailparse php-pear phpunit php-db
-
-if [ -f "$(which composer)" ]; then
-  composer install -o
-else
-  curl --silent --show-error https://getcomposer.org/installer | php && cp composer.phar /usr/local/bin/composer
-  composer install -o
-fi
+docker run --rm -v $(pwd):/app composer/composer:latest install --ignore-platform-reqs
+docker run -it --rm -v $(pwd):/srv -w="/srv/src/resource" huli/grunt:alpine less
 
 # Add developer mode to espocrm/data/config.php
 echo "<?php return ['isDeveloperMode' => true]; ?>" > $CRMDIR/data/config.php
-
-sudo apt-get install -y nodejs npm
-sudo npm install -g grunt
-
-# Building node_modules in local machine before mount for development enviornment
-sudo npm install
-
-# Compiling less to generate compiled css
-grunt less
-
-cd $WORKDIR
-docker build -t php-espocrm:stable php7.2-espocrm
